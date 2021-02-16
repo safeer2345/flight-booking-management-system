bcab80c037463fdafeaaab35bcd5e629298aaa91
safeer2345 <safeer2345@gmail.com>
2021-02-17 01:57:15 +0530

    initial commit

---
+++ src/test/java/com/aitrich/flightbookingsystem/FlightBookingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aitrich.flightbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
